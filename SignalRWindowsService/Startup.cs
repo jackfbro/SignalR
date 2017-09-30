15a9c7611350cabf8a25981a4b3d7b80df91d930
liujie <309217279@qq.com>
2017-09-30 15:50:44 +0800

    redis

--- SignalRWindowsService/Startup.cs
+++ SignalRWindowsService/Startup.cs
@@ -15,9 +15,12 @@ namespace SignalRChatService
         {
             // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
             // Any connection or hub wire up and configuration should go here
+            //SignalR Scaleout with Sql Server
             string sqlConnectionString = @"Server=.;Database=SignalRChat;uid=sa;pwd=123456;";
-            GlobalHost.DependencyResolver.UseSqlServer(sqlConnectionString);          
-            app.UseCors(CorsOptions.AllowAll);
+            GlobalHost.DependencyResolver.UseSqlServer(sqlConnectionString);
+            //SignalR Scaleout with Redis
+            //GlobalHost.DependencyResolver.UseRedis("server", port, "password", "AppName");
+            app.UseCors(CorsOptions.AllowAll); //允许跨域请求
             app.MapSignalR();
         }
     }
